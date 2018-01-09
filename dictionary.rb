34fdda78ced84c5539628cb3fd6e06ff26632927
green <lizard.spock@mail.ru>
2018-01-09 14:15:09 +0300

    add dictionary

---
+++ dictionary.rb
@@ -0,0 +1,8 @@
+@dictionary = [
+  ['will you go?', 'ты пойдешь?'],
+  ['i will go?', 'я пойду?'],
+  ['he will not go', 'он не пойдет'],
+  ['did you go?', 'ты ходил?'],
+  ['i went', 'я ходил']
+
+]
